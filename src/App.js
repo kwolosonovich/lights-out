5ecd46b010051e9ee08c5e1a435e1b5138ec0027
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-11-01 23:56:44 -0700

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Board from "./Board";
 import "./App.css";
 import Home from './Home'
+import Footer from './Footer'
 
 /** Simple app that just shows the LightsOut game. */
 
@@ -15,36 +16,8 @@ function App() {
         <div className="col2">
           <Home />
         </div>
-
         <div className="footer">
-          <div>
-            <a href="https://www.freepik.com/vectors/background">
-              Background vector created by <i>freepik</i> {"; "}
-              <a href="https://www.freepik.com/photos/background">
-                background photo created by <i>bedneyimages</i>
-              </a>
-            </a>
-          </div>
-          <div>
-            {"Icons made by"}{" "}
-            <a
-              href="https://www.flaticon.com/authors/pixel-perfect"
-              title="Pixel perfect"
-            >
-              <i>Pixel perfect</i>
-            </a>{" "}
-            {"and"}{" "}
-            <a
-              href="https://www.flaticon.com/authors/iconixar"
-              title="iconixar"
-            >
-              <i>iconixar</i>
-            </a>{" "}
-            {"from"}{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              <i>www.flaticon.com</i>
-            </a>
-          </div>
+          <Footer />
         </div>
       </div>
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <>
+            <div>
+                <a href="https://www.freepik.com/vectors/background">
+                    Background vector created by <i>freepik</i> {"; "}
+                    <a href="https://www.freepik.com/photos/background">
+                    background photo created by <i>bedneyimages</i>
+                    </a>
+                </a>
+            </div>
+            <div>
+                {"Icons made by"}{" "}
+                <a
+                    href="https://www.flaticon.com/authors/pixel-perfect"
+                    title="Pixel perfect"
+                >
+                    <i>Pixel perfect</i>
+                </a>{" "}
+                {"and"}{" "}
+                <a
+                    href="https://www.flaticon.com/authors/iconixar"
+                    title="iconixar"
+                >
+                    <i>iconixar</i>
+                </a>{" "}
+                {"from"}{" "}
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                    <i>www.flaticon.com</i>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default footer;
